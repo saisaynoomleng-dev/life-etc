d16220ea6c0467592357a7e7fa82aa305aaf6d9b
saisaynoomleng-dev <snoomleng1@cougars.ccis.edu>
2026-03-03 14:48:53 -0500

    finish init

---
+++ src/app/(frontend)/(utility-page)/store-policy/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StorePolicyPage = () => {
+  return <div>StorePolicyPage</div>;
+};
+
+export default StorePolicyPage;
---
+++ src/app/(frontend)/journal/[slug]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const JournalDetail = () => {
+  return <div>JournalDetail</div>;
+};
+
+export default JournalDetail;
